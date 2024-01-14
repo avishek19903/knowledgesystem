b8d7d2b0649c0ae3fe1624106fad84d4cb7f3ec1
Avishek shrestha <avishekshrestha9909@gmail.com>
2024-01-14 23:28:25 +0545

    initialize router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,27 @@
-import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
 
-import Login from './pages/Login';
+import Login from "./pages/Login";
 
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <MantineProvider>
-    <Login />
-  </MantineProvider>
+  return (
+    <MantineProvider>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="*"
+          element={
+            <>
+              <h1 className="text-white">Page Not found!</h1>
+            </>
+          }
+        />
+      </Routes>
+  
+    </MantineProvider>
+  );
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App";
+
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "react-auth-kit";
+import createStore from "react-auth-kit/createStore";
+
+const store = createStore({
+  authName: "_auth",
+  authType: "cookie",
+  cookieDomain: window.location.hostname,
+  cookieSecure: false,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AuthProvider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </React.StrictMode>
+);
